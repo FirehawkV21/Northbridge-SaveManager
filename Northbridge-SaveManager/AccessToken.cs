770f891b76db7587ca810a4c751a9e7e29b58679
Dimitris Trachiotis <acemod13@outlook.com>
2016-03-02 19:16:05 +0200

    Set up OneDrive SDK.

---
+++ Northbridge-SaveManager/AccessToken.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace NorthbridgeLauncher
+{
+    internal class AccessToken
+    {
+            public string Token
+            {
+                get { return _token; }
+                set
+                {
+                    _token = value;
+                    _generationTime = DateTime.Now;
+                }
+            }
+
+            public bool IsValid
+            {
+                get
+                {
+                    if (string.IsNullOrEmpty(_token)) return false;
+                    return (DateTime.Now -
+                            _generationTime).TotalMinutes < 60;
+                }
+            }
+
+            private string _token = string.Empty;
+            private DateTime _generationTime;
+
+        }
+}
---
+++ Northbridge-SaveManager/AuthBrowser.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace NorthbridgeSubSystem
+{
+    public partial class AuthBrowser : Form
+    {
+        private string AccessToken
+        {
+            get { return _accessToken; }
+            set { _accessToken = value; }
+        }
+        private string _accessToken = string.Empty;
+
+        private const string Scope = "wl.skydrive_update";
+
+        // Remember to change this to your ClientID, this one will be
+        // invalid by the time this is published
+        private const string ClientId = "0000000000000000";
+
+        private const string SignInUrl = @"https://login.live.com/
+         oauth20_authorize.srf?client_id={0}&redirect_uri=
+         https://login.live.com/oauth20_desktop.srf&response_type=
+         token&scope={1}";
+        private Timer _closeTimer;
+        public AuthBrowser()
+        {
+            InitializeComponent();
+            StartAuthenticationProcess();
+        }
+        private void StartAuthenticationProcess()
+        {
+            webBrowser1.DocumentCompleted +=
+               AuthenticationBrowserDocumentCompleted;
+            webBrowser1.Navigate(string.Format
+               (SignInUrl, ClientId, Scope));
+        }
+
+        private void AuthenticationBrowserDocumentCompleted(object sender,
+           WebBrowserDocumentCompletedEventArgs e)
+        {
+            if (!e.Url.AbsoluteUri.Contains("#access_token=")) return;
+            var x = e.Url.AbsoluteUri.Split(new[]
+            { "#access_token" }, StringSplitOptions.None);
+            AccessToken = x[1].Split(new[] { '&' })[0];
+            _closeTimer = new Timer { Interval = 500 };
+            _closeTimer.Tick += CloseTimerTick;
+            _closeTimer.Enabled = true;
+        }
+
+        private void CloseTimerTick(object sender, EventArgs e)
+        {
+            _closeTimer.Enabled = false;
+            DialogResult = DialogResult.OK;
+            Close();
+        }
+        private void AuthBrowser_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
